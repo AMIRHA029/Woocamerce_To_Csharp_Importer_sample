7e05a255991d7f376be0a566d3df912bc6773f26
AMIRHA029 <amirha029@gmail.com>
2023-05-29 16:52:08 +0430

    Add project files.

---
+++ Importer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using static Importer.Models.WoocammerceProduct;
+
+string Response = System.IO.File.ReadAllText("Recources\\response.json");
+
+var products = JsonConvert.DeserializeObject<List<Root>>(Response);
+
+products[0].Images[0].DownloadImage(Directory.GetCurrentDirectory() + "\\Recources\\");
+
+Console.WriteLine(products.Count);
+
+Console.ReadKey();
